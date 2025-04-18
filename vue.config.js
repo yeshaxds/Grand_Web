5c0c43853056471151f8a3c5ad62bfd50611658c
yeshaxds <132030326+yeshaxds@users.noreply.github.com>
2025-04-18 18:01:20 +0800

    Delete vue.config.js

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  // 开发服务器配置
-  devServer: {
-    port: 8080,
-    open: true
-  }
-}) 

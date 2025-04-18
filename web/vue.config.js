cdcf6f8abe89b60086fe39e9352285646d98403a
yeshaxds <132030326+yeshaxds@users.noreply.github.com>
2025-04-18 22:27:09 +0800

    Update vue.config.js

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -25,5 +26,27 @@ module.exports = defineConfig({
       .rule('images')
       .test(/\.(png|jpe?g|gif|webp|avif|ico)$/i)
       .set('type', 'asset')
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'monaco-editor': path.resolve(__dirname, 'node_modules/monaco-editor')
+      }
+    },
+    plugins: [
+      new MonacoWebpackPlugin({
+        languages: ['javascript', 'css', 'html', 'typescript', 'json'],
+        filename: 'js/[name].worker.js',
+        features: [
+          'accessibilityHelp', 'bracketMatching', 'caretOperations', 'clipboard',
+          'colorDetector', 'comment', 'contextmenu', 'coreCommands', 'cursorUndo',
+          'dnd', 'find', 'folding', 'fontZoom', 'format', 'gotoLine', 'hover',
+          'inPlaceReplace', 'inspectTokens', 'linesOperations', 'links', 'multicursor',
+          'parameterHints', 'quickCommand', 'quickOutline', 'referenceSearch', 'rename',
+          'smartSelect', 'snippets', 'suggest', 'toggleHighContrast', 'toggleTabFocusMode',
+          'transpose', 'wordHighlighter', 'wordOperations', 'wordPartOperations'
+        ]
+      })
+    ]
   }
 }) 

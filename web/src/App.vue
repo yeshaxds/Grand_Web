7663d2ec5637e24b8c9017fe8295d51b5fe6b4f7
yeshaxds <132030326+yeshaxds@users.noreply.github.com>
2025-04-18 21:37:10 +0800

    Update App.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -9,6 +9,7 @@
       <router-link to="/languages">编程语言</router-link>
       <router-link to="/stacks">技术栈</router-link>
       <router-link to="/resources">学习资源</router-link>
+      <router-link to="/code-editor">代码编辑器</router-link>
       <router-link to="/chat">AI对话</router-link>
       <router-link to="/about">关于我们</router-link>
     </div>
